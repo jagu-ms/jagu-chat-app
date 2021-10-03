f4bca83181f681d85fff4245e2a4169cf29fcc96
jagu-ms <mohamedseddiki20@gmail.com>
2021-10-03 14:04:19 +0200

    Uploading to heroku

--- server/app.js
+++ server/app.js
@@ -34,22 +34,16 @@ app.use('/api/account', require('./routes/account'));
 
 // error middlewares
 app.use((err, req, res, next) => {
-    if (!err) return next();
     if(err.name === 'MongoError' || err.name === 'ValidationError' || err.name === 'CastError'){
         // that means there are some data errors
         err.status = 422;
     }
-    res.status(err.status || 500).json({message: err.message || "we have some error eccured."});
-});
-
-app.use((err, req, res, next) => {
     if(req.get('accept').includes('json')){
-        return next(createError(404));
+        res.status(err.status || 500).json({message: err.message || "we have some errors eccured."});
+    } else {
+        res.status(err.status || 500).sendFile(path.join(__dirname, 'public', 'index.html'));
     }
-    res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'))
 });
-
-
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true } , (err) => {
     if(err) throw err;
